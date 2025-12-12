cf4f48ee66cbcda9e14d42c61aada26815d249da
faodiallo001 <faodiallo001@gmail.com>
2025-12-11 23:22:57 -0300

    lang.js

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -5,21 +5,30 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   function detectLanguage() {
-    const lang = navigator.language || navigator.userLanguage;
+    const lang = navigator.language.toLowerCase();
 
     if (lang.startsWith("fr")) return "fr";
-    if (lang.startsWith("pt")) return "pt"; // Portugais Brésil
+    if (lang.startsWith("pt")) return "pt";
     if (lang.startsWith("es")) return "es";
-    return "en"; // default English
+    return "en"; // default
   }
 
   const translations = await loadTranslations();
   const userLang = detectLanguage();
 
+  // Remplace tout ce qui a data-i18n
   document.querySelectorAll("[data-i18n]").forEach((el) => {
     const key = el.getAttribute("data-i18n");
     if (translations[userLang][key]) {
       el.textContent = translations[userLang][key];
+    }
+  });
+
+  // Remplace les placeholders
+  document.querySelectorAll("[data-i18n-placeholder]").forEach((el) => {
+    const key = el.getAttribute("data-i18n-placeholder");
+    if (translations[userLang][key]) {
+      el.placeholder = translations[userLang][key];
     }
   });
 });
